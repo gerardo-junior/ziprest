92edcf0030d68c1052b28eee5ae4c0c0cb93ce24
Gerardo Junior <me@gerardo-junior.com>
2021-01-16 11:25:33 -0300

    First commit

---
+++ src/main/java/com/gerardojunior/ziprest/ZiprestApplication.java
@@ -0,0 +1,13 @@
+package com.gerardojunior.ziprest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZiprestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZiprestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gerardojunior/ziprest/ZiprestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gerardojunior.ziprest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZiprestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
