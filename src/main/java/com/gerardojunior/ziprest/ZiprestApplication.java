5efcbed834c1aaef72ab8ebbd9c2f1e0457fd4fa
Gerardo Junior <me@gerardo-junior.com>
2021-01-19 09:08:20 -0300

    Adding service to resolver zipcode over viacep

--- src/main/java/com/gerardojunior/ziprest/ZiprestApplication.java
+++ src/main/java/com/gerardojunior/ziprest/ZiprestApplication.java
@@ -2,8 +2,10 @@ package com.gerardojunior.ziprest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class ZiprestApplication {
 
 	public static void main(String[] args) {
